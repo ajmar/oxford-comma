f14e9cdee3704227429cca142b036c267b2e5aa7
ajmar <amarusin18@gmail.com>
2020-11-21 07:43:52 -0700

    commit tree 244a23536afc6c3bcff9e95efcddf0d02af6b3f1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
 
+    if array.length == 1
+
+        array.join
+
+    elsif array.length == 2
+
+        array.insert(-2, " and ")
+
+        array.join
+
+    else
+
+    newstring = array[0...-1].join(", ")
+
+    newstring << ", and " + array[-1]
+
+    newstring
+
+
+    end
+
 end
